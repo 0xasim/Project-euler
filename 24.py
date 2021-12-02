030531b38c94a8f68c8f9056294f96c0277ae97e
Asim <asimalilatif@gmail.com>
2021-12-02 15:00:34 +0500

    heaps algorithm

--- 24.py
+++ 24.py
@@ -1,9 +1,29 @@
-def lexicograph(o):
-  lexgraph = list()
-  perm = list()
-  for char in o:
-    pass
+from utils import *
 
-  return sorted(lexgraph)
+# Heap's Algorithm for generating all possible permutations
+def lexicograph(o, f='generate'):
+  permutations = list()
+  def generate(k: int, A: list):
+    if k == 1: permutations.append(''.join(A))
+    else:
+      for i in range(k):
+        generate(k-1, A)
+        if k % 2 == 0:
+          A[i], A[k-1] = A[k-1], A[i]
+        else: A[0], A[k-1] = A[k-1], A[0]
 
-print(lexicograph('012'))
+  def generate2(k: int, A: list):
+    if k == 1: permutations.append(''.join(A))
+    else:
+      generate2(k-1, A)
+      for i in range(k-1):
+        if k % 2 == 0:
+          A[i], A[k-1] = A[k-1], A[i]
+        else: A[0], A[k-1] = A[k-1], A[0]
+        generate2(k-1, A)
+
+  eval(f'{f}(len(o), list(o))')
+  return (permutations)
+
+seq = '0123456789'
+print(lexicograph(seq)[10**6-1])
