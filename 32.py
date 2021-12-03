90a6b3b90fddee248c32589126e431f747b4b525
Asim <asimalilatif@gmail.com>
2021-12-03 19:41:47 +0500

    tighter limits

--- 32.py
+++ 32.py
@@ -3,9 +3,10 @@ ispandigital = lambda prod, mpcant, mplier:\
 
 def allpandigitals():
   prods = set()
-  for i in range(10000):
-    for j in range(1000):
-      if ispandigital(i*j, i, j) and i*j not in prods: prods.add(i*j)
+  for i in range(2000):
+    for j in range(500):
+      if len(str(i*j)) == 4 and i*j not in prods \
+                      and ispandigital(i*j, i, j): prods.add(i*j)
   return sum(prods)
   
 from utils import call
