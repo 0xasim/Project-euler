2d28ccea10a47fd03517d0e5c9332c6d539083dc
Asim <asimalilatif@gmail.com>
2021-12-03 12:34:57 +0500

    27 done

--- 27.py
+++ 27.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 def quadratic_primes():
   qf = "n**2 + a*n + b"
   max_primes = 0
@@ -5,9 +7,8 @@ def quadratic_primes():
 
   for a in range(-1000, 1000):
     for b in range(-1000, 1000+1):
-      print(a, b)
       primes = 0
-      for n in range(100):
+      for n in count():
         if not isprime(eval(qf)): break
         primes += 1
       if primes > max_primes:
@@ -15,9 +16,16 @@ def quadratic_primes():
         max_primes_vars = {"a" : a, "b" : b}
         print (max_primes, max_primes_vars)
 
-  return (max_primes, max_primes_vars)
+  return (max_primes_vars['a'] * max_primes_vars['b'])
 
-isprime = lambda n: True if all([n % i != 0 for i in range(1, int(n/2)+1)]) else False
+def isprime(n):
+  prime = True
+  if n <=1: return False
+  for i in range(2, 3000):
+    if n % i == 0 and n != i:
+      prime = False
+      break
+  return prime
 
 from utils import call
 call(quadratic_primes)
