3fa07806001fb00855d5b0b37a23c5706d6454dc
Asim <asimalilatif@gmail.com>
2021-12-06 00:45:32 +0500

    An attempt at 46 using prime sieve

--- 46.py
+++ 46.py
@@ -1,12 +1,32 @@
 # Here to prove Goldbach wrong
-expr = 'oc == pr + 2 * n**2'
+expr = 'eoc == epr + 2 * n**2'
 import math
 def primeSieve(stop: int):
-  pool = [True for _ in range(stop + 1)]
+  pool = [True for _ in range(stop)]
   pool[0], pool[1] = False, False
-  for p in range(2, int(math.sqrt(stop) + 1)):
-    for q in range(2, stop + 1):
-      if (pq := p * q) <= stop: pool[pq] = False
-  print(pool)
+  for i in range(2, int(math.sqrt(stop) + 1)):
+    if pool[i]:
+      jays = (j for x in range(stop) if (j:= i**2 + x*i) < stop)
+      for j in jays: pool[j] = False
+  return ({
+         'primes': [zi for zi, z in enumerate(pool) if z],
+            'oc' : {yi for yi, y in enumerate(pool) if not y and yi % 2} # odd composites
+    })
 
-primeSieve(10)
+def falseConj():
+  ps = primeSieve(10**7)
+  primes, oc = ps['primes'], ps['oc']
+  ocied = [False for i in range(len(oc))]
+  for eoci, eoc in enumerate(oc):
+    for epr in primes:
+      for n in range(1,100):
+        if (ev := eval(expr) in oc): 
+          ocied[eoci] = True
+          break
+      if ev > eoc: break  #eval > odd composite
+    if ocied[eoci]: break
+  return [(oi, o) for o in enumerate(ocied) if not o]
+
+
+from utils import call
+call(falseConj)
