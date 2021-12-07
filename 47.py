407ae11aeecb4e3d26df5e7cb4fd6717063cdc5f
Asim <asimalilatif@gmail.com>
2021-12-07 22:26:20 +0500

    major refactor, primesieve for factoriz

--- 47.py
+++ 47.py
@@ -2,41 +2,19 @@
 def consDPF(nf):
   i = 0
   while True:
-    if all([DPF(c) == nf for c in range(i, i + nf)]):
+    if all([len(set(factorize(c))) == nf for c in range(i, i + nf)]):
       return [i+a for a in range(nf)]
     i += 1
-
-# Distinct Prime Factors (DPF)
-def DPF(n: int):
-  dist_fact = set(factorize(n))
-  if dist_fact:
-    allprimes = all([inPS(f) for f in dist_fact])
-    if not allprimes: return None
-    return len(dist_fact)
-  return None
 
 import functools
 @functools.cache
 def factorize(n: int):
-  factors = list()
-  def calc(x: int):
-    if x <= 1: return x
-    for i in range(2, int(x/2) + 1):
-      if x % i == 0:
-        factors.append(i)
-        return calc(x / i)
-    factors.append(int(x))
-  calc(n)
-  return factors
-
-import math
-@functools.cache
-def isprime(y: int):
-  if y <= 1: return False
-  for i in range(2, int(math.sqrt(y) + 1)):
-    if y % i == 0:
-      return False
-  return True
+  sortedSP = sorted(sieve_primes)
+  fs = list()
+  for i in sortedSP:
+    if n % i == 0:
+      fs.append(i)
+  return fs
 
 import math
 def primeSieve(stop: int):
@@ -49,10 +27,5 @@ def primeSieve(stop: int):
   return [zi for zi, z in enumerate(pool) if z]
    
 from utils import call
-sieve_primes = set(call(primeSieve, 10**4, pout=False))
-inPS = lambda pc: pc in sieve_primes
+sieve_primes = set(call(primeSieve, int(1e3), pout=False))
 call(consDPF, 4)
-
-
-
-
