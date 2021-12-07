f26d5a2b9e434bef0c21c34984466a3644f77f8c
Asim <asimalilatif@gmail.com>
2021-12-07 15:09:37 +0500

    goldbach conjecture proven wrong

--- 46.py
+++ 46.py
@@ -1,5 +1,4 @@
 # Here to prove Goldbach wrong
-expr = 'eoc == epr + 2 * n**2'
 import math
 def primeSieve(stop: int):
   pool = [True for _ in range(stop)]
@@ -10,23 +9,22 @@ def primeSieve(stop: int):
       for j in jays: pool[j] = False
   return ({
          'primes': [zi for zi, z in enumerate(pool) if z],
-            'oc' : {yi for yi, y in enumerate(pool) if not y and yi % 2} # odd composites
-    })
-
-def falseConj():
-  ps = primeSieve(10**7)
-  primes, oc = ps['primes'], ps['oc']
-  ocied = [False for i in range(len(oc))]
-  for eoci, eoc in enumerate(oc):
-    for epr in primes:
-      for n in range(1,100):
-        if (ev := eval(expr) in oc): 
-          ocied[eoci] = True
-          break
-      if ev > eoc: break  #eval > odd composite
-    if ocied[eoci]: break
-  return [(oi, o) for o in enumerate(ocied) if not o]
-
+            'oc' : [yi for yi, y in enumerate(pool) if not y and yi % 2] # odd composites
+          })
 
 from utils import call
+def falseConj():
+  ps = call(primeSieve, 10**4 , pout=False)
+  primes, oc = ps['primes'], ps['oc'][1:] 
+
+  for eoc in oc:
+    found = False
+    for epr in primes:
+      if found: break
+      for n in range(1,100):
+        if eoc == epr + 2 * n**2: 
+          found = True
+          break
+    if not found: return eoc
+
 call(falseConj)
