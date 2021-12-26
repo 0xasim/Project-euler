112ee597a13067fafdb3ddb10d704f2d296292e0
Asim <asimalilatif@gmail.com>
2021-12-26 23:02:23 +0500

    input can be chosen to not be printed

--- utils.py
+++ utils.py
@@ -6,11 +6,13 @@ def timeMe(f, *args):
   _t1 = time.time() 
   return (_t1 - _t0, retval)
   
-def call(func, *args, pout=True):
+def call(func, *args, pin=True, pout=True):
   _t, retval = timeMe(func, *args)
-  pval = retval
-  if not pout: pval = ''
-  print(f"Function: {func.__name__}\n\tInput:\t{args}\n\tOutput:\t{pval}\n\tExecution time: {_t}\n")
+  poval = retval
+  pival = args
+  if not pout: poval = ''
+  if not pin: pival = ''
+  print(f"Function: {func.__name__}\n\tInput:\t{pival}\n\tOutput:\t{poval}\n\tExecution time: {_t}\n")
   return retval
 
 def callDec(func):
