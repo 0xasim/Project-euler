7b5922e1050fc709b63c6b066b1c2da6d2bcd18b
Asim <asimalilatif@gmail.com>
2021-12-26 23:02:53 +0500

    finally

--- 51.py
+++ 51.py
@@ -2,34 +2,27 @@ from primes import *
 import numpy as np
 import itertools
 
-primeslist = set(psieve_list(10**6))
-max_primes = 0
+def prime_dig_replace(primeslist, lim=8):
+  for i in itertools.count(56003):
+    na = np.array([int(px) for px in str(i)]) # [3 8 1 7]..
+    for replace_count in range(1, len(str(i))): # Num of chars to replace (1, 2 and 3)
+      places = list(itertools.combinations(np.arange(len(na)), replace_count))
+      for ep in places: # [0 1 2], [0 1 3], [0 2 3], ....,[1 2 3]
+        num_of_primes = 0
+        plist = list()
+        drange = range(10)
+        if ep[0] == 0: drange = range(1,10) 
+        for d in drange:
+          tna = na.copy()
+          tna[list(ep)] = [d]*replace_count
+          tnum = int(''.join([str(tt) for tt in tna]))
+          if tnum in primeslist:
+            num_of_primes += 1
+            plist.append(tnum)
+        if num_of_primes == lim:
+          return min(plist)
 
-# for i in itertools.count(10):
-iters = [56003, 120303] 
-#for i in range(56000, 56003):
-for i in iters:
-  if i%1000==0: print(i)
-  na = np.array([int(px) for px in str(i)]) # [3 8 1 7]..
-  print(i, na)
-  for replace_count in range(1, len(str(i))): # Num of chars to replace (1, 2 and 3)
-    print(f'\treplace_count = {replace_count}')
-    places = list(itertools.combinations(np.arange(len(na)), replace_count))
-    print(f'\tplaces {places}')
-    for ep in places: # [0 1 2], [0 1 3], [0 2 3], ....,[1 2 3]
-      num_of_primes = 0
-      print(f'\t\tep {ep}')
-      drange = range(10)
-      if ep[0] == 0: drange = range(1,10) 
-      for d in drange:
-        tna = na.copy()
-        tna[list(ep)] = [d]*replace_count
-        print(f'\t\t\ttna {tna}')
-        if int(''.join([str(tt) for tt in tna])) in primeslist:
-          print(f'\t\t\t\t this tna is a prime')
-          num_of_primes += 1
-      # print(f'num_of_primes {num_of_primes}')
-      if num_of_primes > max_primes:
-        max_primes = num_of_primes
-        print(i, max_primes, 'found'*10)
+from utils import call
+primeslist = set(call(psieve_list, 10**6, pout=False))
+call(prime_dig_replace, primeslist, pin=False)
 
