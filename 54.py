a88977b2cef61d19f4abc0cd0d7c3296bbc43b94
Asim <asimalilatif@gmail.com>
2021-12-28 12:56:13 +0500

    so done with poker

--- 54.py
+++ 54.py
@@ -2,17 +2,50 @@ with open('data/p054_poker.txt') as f:
   poker_hands = f.read().split('\n')[:-1]
   poker_hands = [p.split() for p in poker_hands]
 
+kinds = '123456789JQKA'
+suits = 'CDHS'
 def rank_a_hand(hand):
   h0, h1 = [c[0] for c in hand], [c[1] for c in hand]
-  if all([o.isnumeric() for o in h0]) and int(sorted(h0)[-1]) == int(sorted(h0)[0])+ 4:
-    print('Straight Flush:2')
+  sh0, sh1 = sorted(h0), sorted(h1)
+  # Royal flush | 0
+  # Not possible because there is no 10
+  # ------------------------------------------------
+  # Straight flush | 0
+  if all([o.isnumeric() for o in h0]) and int(sh0[-1]) == int(sh0[0]) + 4\
+      and any([all([p == q for p in sh1]) for q in suits]):
+    return 9
+  # Four of a kind | 0
+  if any([len([1 for s in sh0 if s == c]) == 4 for c in kinds]):
+    return 8
+  # Full House | 2
+  if any([len([1 for s in sh0 if s == c]) == 3 for c in kinds]) and len(set(sh0)) == 2:
+  # if not any([o.isnumeric() for o in h0]):
+    return 7
+  # Flush | 2
+  if any([len([1 for m in sh1 if m == x]) == 5 for x in suits]):
+    return 6
+  # Straight | 31
+  if all([o.isnumeric() for o in h0]) and int(sh0[-1]) == int(sh0[0]) + 4:
+    return 5
+  # Three of a kind | 33
+  if any([len([1 for s in sh0 if s == c]) == 3 for c in kinds]):
+    return 4
+  # Two of a pair
+  # Pair
+  # High card
+  return 0
 
 def winner(play): 
   assert len(play) == 10
   p1h = play[:5]
   p2h = play[5:]
-  rank_a_hand(p1h)
-  rank_a_hand(p2h)
+  r1 = rank_a_hand(p1h)
+  r2 = rank_a_hand(p2h)
+  print(r1, r2, end=', ')
+  if r1 > r2: return 1
+  elif r2 > r1: return 2
+  else:
+    return "eq"
 
 for h in poker_hands:
-  winner(h)
+  print(winner(h), end=', ')
