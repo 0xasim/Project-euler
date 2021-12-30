532c3a1e0a0b32081ab3e1c5b803fc52aad4d8df
Asim <asimalilatif@gmail.com>
2021-12-30 15:12:28 +0500

    refactor, still unsolved

--- 54.py
+++ 54.py
@@ -1,9 +1,6 @@
+import numpy as np
 
-with open('data/p054_poker_test.txt') as f:
-  poker_hands = f.read().split('\n')[:-1]
-  poker_hands = [p.split() for p in poker_hands]
-
-kinds = '123456789JQKA' # len(kinds) = 13
+kinds = '123456789JQKA' # len(kinds) == 13
 suits = 'CDHS'
 def rank_a_hand(hand, h0, h1):
   sh0, sh1 = sorted(h0), sorted(h1)
@@ -18,9 +15,9 @@ def rank_a_hand(hand, h0, h1):
     # print(sk, len(sk))
     return [20, sk[0]]
   # Full House | 1
-  if any([len(sk := [s for s in sh0 if s == c]) == 3 for c in kinds]) and len(set(sh0)) == 2:
-    # print(sk, len(sk))
-    return [19, sk]
+  for c in kinds:
+    if len(sk := [s for s in sh0 if s == c]) == 3 and len(set(sh0)) == 2:
+      return [19, sk[0]]
   # Flush | 2
   if any([len([1 for m in sh1 if m == x]) == 5 for x in suits]):
     return [18]
@@ -28,9 +25,12 @@ def rank_a_hand(hand, h0, h1):
   if all([o.isnumeric() for o in h0]) and int(sh0[-1]) == int(sh0[0]) + 4:
     return [17]
   # Three of a kind | 33 
-  if any([len([1 for s in sh0 if s == c]) == 3 for c in kinds]):
+  if any([len(sk:= [1 for s in sh0 if s == c]) == 3 for c in kinds]):
     return [16]
   # Two Pairs | 80
+  # for c in kinds:
+  #   if sum(len(pv := [s for s in sh0 if s == c]) == 2) == 2:
+
   if sum([len([1 for s in sh0 if s == c]) == 2 for c in kinds]) == 2:
     return [15]
   # One Pair | 1171
@@ -45,12 +45,14 @@ def highCardV(p1h0, p2h0):
   for k in kinds:
     p1i, p1 = [k for k in enumerate(kinds) if k[1] in p1h0][-1]
     p2i, p2 = [k for k in enumerate(kinds) if k[1] in p2h0][-1]
-    if p1i > p2i: return 1
-    elif p2i > p1i: return 2
-    p1h0.remove(p1)
-    p2h0.remove(p2)
+    if p1 == p2:
+      p1h0.remove(p1)
+      p2h0.remove(p2)
+      continue
+    return np.where(p1i > p2i, 1, 2)
 
 def winner(play): 
+  global p1_wins, p2_wins
   assert len(play) == 10
   p1h, p2h = play[:5], play[5:]
   p1h0, p1h1 = [c[0] for c in p1h], [c[1] for c in p1h]
@@ -64,6 +66,19 @@ def winner(play):
   return highCardV(p1h0[:], p2h0[:])
   return "equal"
 
-for h in poker_hands:
-  print(winner(h))
+def euler_54():
+  p1_wins, p2_wins = 0, 0
+  with open('data/p054_poker.txt') as f:
+    poker_hands = f.read().split('\n')[:-1]
+    poker_hands = [p.split() for p in poker_hands]
+  for h in poker_hands:
+    won = winner(h)
+    if won == 1: p1_wins += 1
+    else: p2_wins += 1
+    print(won)
+  return (f'p1_wins: {p1_wins}, p2_wins:{p2_wins}')
+print(euler_54())
+
+
+
 
