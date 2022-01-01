5aeb4d472f6ddde6a44c2d87e3934cca68c5a955
Asim <asimalilatif@gmail.com>
2022-01-01 14:53:43 +0500

    i give up on 54

---
+++ 54.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.IO;
+
+namespace euler {
+    class Problem54 {
+        enum Suit { Heart, Spade, Club, Diamond };
+             
+        Card[] hand1 = new Card[5];
+        Card[] hand2 = new Card[5];
+        string filename = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\input.txt";
+        StreamReader reader;
+
+        public static void Main(string[] args) {
+            new Problem54().BruteForce();
+        }
+
+
+        public void BruteForce() {
+            Stopwatch clock = Stopwatch.StartNew();
+            int result = 0;
+
+            while (readHands()) {
+                Array.Sort(hand1);
+                Array.Sort(hand2);
+                if (DidPlayer1Win(hand1, hand2)) result++;                
+            }
+            
+            clock.Stop();
+            Console.WriteLine("Player 1 won {0} hands", result);
+            Console.WriteLine("Solution took {0} ms", clock.ElapsedMilliseconds);
+        }
+
+
+        private bool DidPlayer1Win(Card[] hand1, Card[] hand2) {
+            if (IsRoyalFlush(hand1) != IsRoyalFlush(hand2)) return IsRoyalFlush(hand1) > IsRoyalFlush(hand2);            
+            if (IsStraightFlush(hand1) != IsStraightFlush(hand2)) return IsStraightFlush(hand1) > IsStraightFlush(hand2);                        
+            if (IsFourOfAKind(hand1) != IsFourOfAKind(hand2)) return IsFourOfAKind(hand1) > IsFourOfAKind(hand2);                        
+            if (IsFullHouse(hand1, 1) != IsFullHouse(hand2, 1)) return IsFullHouse(hand1,1) > IsFullHouse(hand2,1);            
+            if (IsFullHouse(hand1,2) != IsFullHouse(hand2,2)) return IsFullHouse(hand1,2) < IsFullHouse(hand2,2);                                      
+            if (IsFlush(hand1) != IsFlush(hand2)) return IsFlush(hand1) > IsFlush(hand2);            
+            if (IsStraight(hand1) != IsStraight(hand2)) return IsStraight(hand1) > IsStraight(hand2);                      
+            if (IsThreeOfAKind(hand1) != IsThreeOfAKind(hand2)) return IsThreeOfAKind(hand1) > IsThreeOfAKind(hand2);            
+            if (IsTwoPairs(hand1, 1) != IsTwoPairs(hand2, 1)) return IsTwoPairs(hand1, 1) > IsTwoPairs(hand2, 1);            
+            if (IsTwoPairs(hand1, 2) != IsTwoPairs(hand2, 2)) return IsTwoPairs(hand1, 2) > IsTwoPairs(hand2, 2);            
+            if (IsOnePair(hand1) != IsOnePair(hand2)) return IsOnePair(hand1) > IsOnePair(hand2);            
+            if (IsHighCard(hand1,0) != IsHighCard(hand2,0)) return IsHighCard(hand1,0) > IsHighCard(hand2,0);            
+            if (IsHighCard(hand1,1) != IsHighCard(hand2,1)) return IsHighCard(hand1,1) > IsHighCard(hand2,1);            
+            if (IsHighCard(hand1,2) != IsHighCard(hand2,2)) return IsHighCard(hand1,2) > IsHighCard(hand2,2);            
+            if (IsHighCard(hand1,3) != IsHighCard(hand2,3)) return IsHighCard(hand1,3) > IsHighCard(hand2,3);    
+
+            return false;
+        }
+        
+        private static int IsStraight(Card[] h) {                                               
+            for (int i = 0; i < 4; i++) {
+                if (h[i].Value != h[i + 1].Value - 1) {
+                    return -1;                    
+                }
+            }
+            return h[4].Value;
+        }
+
+        private static int IsFlush(Card[] h) {
+            for (int i = 0; i < 4; i++) {
+                if (h[i].Suit != h[i + 1].Suit) {
+                    return -1;                    
+                }
+            }
+            return h[4].Value;
+        }
+
+        private static int IsStraightFlush(Card[] h) {
+            if (IsFlush(h) > 0 && IsStraight(h) > 0) {
+                return h[4].Value;
+            }
+            return -1;                     
+        }
+
+        private static int IsRoyalFlush(Card[] h) {
+            if (IsFlush(h) == 14) return 14;
+            return -1;
+        }
+
+        private static int IsFourOfAKind(Card[] h) {
+            if (h[0].Value == h[1].Value &&
+                h[1].Value == h[2].Value &&
+                h[2].Value == h[3].Value)
+                return h[0].Value;
+            if (h[1].Value == h[2].Value &&
+                h[2].Value == h[3].Value &&
+                h[3].Value == h[4].Value)
+                return h[1].Value;
+
+            return -1;
+
+        }
+
+        private static int IsFullHouse(Card[] h, int set) {            
+            if(set == 1 &&
+               h[0].Value == h[1].Value &&
+               h[1].Value == h[2].Value &&
+               h[3].Value == h[4].Value)
+                return h[0].Value;
+            if (set == 2 &&
+                h[0].Value == h[1].Value &&
+               h[1].Value == h[2].Value &&
+               h[3].Value == h[4].Value)
+                return h[4].Value;
+            if(set == 1 &&
+               h[0].Value == h[1].Value &&
+               h[2].Value == h[3].Value &&
+               h[3].Value == h[4].Value)
+                return h[4].Value;
+            if (set == 2 &&
+                h[0].Value == h[1].Value &&
+               h[2].Value == h[3].Value &&
+               h[3].Value == h[4].Value)
+                return h[0].Value;
+            return -1;
+        }
+
+        private static int IsThreeOfAKind(Card[] h) {
+            if (h[0].Value == h[1].Value &&
+               h[1].Value == h[2].Value)
+                return h[0].Value;
+
+            if (h[1].Value == h[2].Value &&
+               h[2].Value == h[3].Value)
+                return h[1].Value;
+
+            if (h[2].Value == h[3].Value &&
+               h[3].Value == h[4].Value)
+                return h[2].Value;
+
+            return -1;
+        }
+
+        private static int IsTwoPairs(Card[] h, int set) {
+            if (set == 1 &&
+                h[0].Value == h[1].Value &&
+                h[2].Value == h[3].Value)
+                return h[0].Value;
+            if (set == 2 &&
+                h[0].Value == h[1].Value &&
+                h[2].Value == h[3].Value)
+                return h[2].Value;                        
+            if (set == 1 &&
+                h[0].Value == h[1].Value &&
+                h[3].Value == h[4].Value)
+                return h[0].Value;
+            if (set == 2 &&
+                h[0].Value == h[1].Value &&
+                h[3].Value == h[4].Value)
+                return h[3].Value;
+
+            if (set == 1 &&
+                h[1].Value == h[2].Value &&
+                h[3].Value == h[4].Value)
+                return h[1].Value;
+            if (set == 2 &&
+                h[1].Value == h[2].Value &&
+                h[3].Value == h[4].Value)
+                return h[3].Value;
+            return -1;
+        }
+
+        private static int IsOnePair(Card[] h) {
+            if (h[0].Value == h[1].Value) return h[0].Value;
+            if (h[1].Value == h[2].Value) return h[1].Value;
+            if (h[2].Value == h[3].Value) return h[2].Value;
+            if (h[3].Value == h[4].Value) return h[3].Value;
+                        
+            return -1;
+        }
+
+        private static int IsHighCard(Card[] h, int card) {            
+            return h[4-card].Value;
+        }
+        
+        private bool readHands() {
+            if (reader == null) reader = new StreamReader(filename);
+
+
+            string line;
+            string[] linePieces;
+            line = reader.ReadLine();
+            if (line == null) return false;
+            linePieces = line.Split(' ');
+
+            for (int i = 0; i < 5; i++) {
+                hand1[i] = new Card();
+                hand1[i].Value = getCardValue(linePieces[i].Substring(0, 1));
+                hand1[i].Suit = getSuit(linePieces[i].Substring(1, 1));
+            }
+
+            for (int i = 0; i < 5; i++) {
+                hand2[i] = new Card();
+                hand2[i].Value = getCardValue(linePieces[i + 5].Substring(0, 1));
+                hand2[i].Suit = getSuit(linePieces[i + 5].Substring(1, 1));
+            }
+
+            return true;
+        }
+
+        private static Suit getSuit(string suit) {
+            if (suit == "D") return Suit.Diamond;
+            if (suit == "C") return Suit.Club;
+            if (suit == "H") return Suit.Heart;
+            return Suit.Spade;
+        }
+
+        private static int getCardValue(string value) {
+
+            int intvalue;
+            if (Int32.TryParse(value, out intvalue)) return intvalue;
+
+            if (value == "T") return 10;
+            if (value == "J") return 11;
+            if (value == "Q") return 12;
+            if (value == "K") return 13;
+            if (value == "A") return 14;
+
+            return 0;
+        }
+
+        private class Card : IComparable {
+            public Suit Suit { get; set; }
+            public int Value { get; set; }
+
+            public int CompareTo(object o) {
+                if (o is Card) {
+                    Card c = (Card)o;
+                    if (Value < c.Value)
+                        return -1;
+                    else if (Value > c.Value)
+                        return 1;
+                    return 0;
+                }
+                throw new ArgumentException("Object is not a Card");
+            }
+        }
+    }
+}
--- 54.py
+++ 54.py
@@ -1,18 +1,18 @@
-kinds = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] # 14 kinds
+
+kinds = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] # 13 kinds
 suits = 'CDHS'  # 4 Suits
 revKind = dict((k, 0) for k in kinds)
 
 from collections import Counter
-def rank_a_hand(hand, h0, h1):
+def rank_a_hand(h0, h1):
   sh0, sh1 = sorted(h0), sorted(h1)
-  sh0C = Counter(sh0)
-  sh1C = Counter(sh1)
+  sh0C, sh1C = Counter(sh0), Counter(sh1)
   pairs, three, fours = [[i for i in sh0C if sh0C[i] == j] for j in range(2, 5)]
   consec = all([o.isnumeric() for o in sh0]) and int(sh0[-1]) == int(sh0[0]) + 4
   sameSuit = [sh1C[i] for i in sh1C if sh1C[i] == 5]
 
-  # 22, Royal flush       | 0 |
-  if sorted(kinds) == sh0:
+  # 22, Royal flush       | 1 |
+  if sorted(['A','K','Q','J','10']) == sh0:
     return [22]
   # 21, Straight flush    | 0 |
   if any(sameSuit) and consec:
@@ -22,7 +22,7 @@ def rank_a_hand(hand, h0, h1):
     return [20, highV(fours)]
   # 19, Full House        | 2 |
   if any(three) and any(pairs):
-    return [19, [three, pairs]]
+    return [19, [highV(three), highV(pairs)]]
   # 18, Flush             | 2 |
   if any(sameSuit):
     return [18]
@@ -34,16 +34,15 @@ def rank_a_hand(hand, h0, h1):
     return [16, highV(three)]
   # 15, Two Pairs         | 101 |
   if len(pairs) == 2:
+    # print(pairs)
     return [15, highV(pairs)]
   # 14, One Pair          | 825 |
   if len(pairs) == 1:
     return [14, highV(pairs)]
-  # 0-13, highest card    | 482 + 362 + 287 + 190 + 148 + 119 + 88 + 68 + 69 + 68 + 64 + ...
-  return [highV(sh0)]
+  # 0-12, highest card    | 482 + 362 + 287 + 190 + 148 + 119 + 88 + 68 + 69 + 68 + 64 + ...
+  return [highV(sh0)[0], highV(sh0)[1:]]
 
-def highV(h0):
-  if type(h0) != list: h0 = list(h0)
-  return [l[0] for l in enumerate(kinds) if l[1] in h0][-1]
+highV = lambda h0: [l[0] for l in enumerate(kinds) if l[1] in h0][::-1]
 
 import numpy as np
 def highCardV(p1h0, p2h0):
@@ -62,12 +61,13 @@ def winner(play):
   p1h0, p1h1 = [c[0] if c[0] != 'T' else '10' for c in p1h], [c[1] for c in p1h]
   p2h0, p2h1 = [c[0] if c[0] != 'T' else '10' for c in p2h], [c[1] for c in p2h]
 
-  r1 = rank_a_hand(p1h, p1h0, p1h1)
-  r2 = rank_a_hand(p2h, p2h0, p2h1)
-  print(r1, r2, end =' ')
+  r1 = rank_a_hand(p1h0, p1h1)
+  r2 = rank_a_hand(p2h0, p2h1)
+  print(r1, r2, end ='')
   if r1 > r2: return 1
   elif r2 > r1: return 2
-  return highCardV(p1h0[:], p2h0[:])
+  print('comparing high '*5)
+  return highCardV(p1h0, p2h0)
 
 def euler_54(fname):
   p1_wins, p2_wins = 0, 0
@@ -76,8 +76,7 @@ def euler_54(fname):
     poker_hands = [p.split() for p in poker_hands]
 
   for h in poker_hands:
-    won = winner(h)
-    if won == 1: p1_wins += 1
+    if (won := winner(h)) == 1: p1_wins += 1
     else: p2_wins += 1
     print(won)
   return (f'p1_wins: {p1_wins}, p2_wins:{p2_wins}')
