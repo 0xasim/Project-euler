1ba914f7456515db524c7caf3b92ba267477ccd2
Asim <asimalilatif@gmail.com>
2022-01-09 17:30:16 +0500

    dumbest solution ever but i wanted a spiral; taken 15 min to compute

--- 51-100/58.py
+++ 51-100/58.py
@@ -1,13 +1,11 @@
 import numpy as np
 import math
+import sympy
 import sys
 sys.path.append('../algorithms')
 from primes import *
 
 def spiral_primes():
-  primes = psieve_list(16573040+100000)
-  print(primes[-1])
-  primes = set(primes)
   spiral = [[1]]
   n_diag_primes, n = 0, 1
 
@@ -26,22 +24,22 @@ def spiral_primes():
       c += n + 1 + 1
       c_diags.extend([spiral[0][0], spiral[0][-1]])
       # top-left -> bottom-left
-      ta = [c+x for x in range(1, n+1)][::-1]
-      for x in range(1, n+1):
-        spiral[-x].insert(0, ta[x-1])
+      # ta = [c+x for x in range(1, n+1)][::-1]
+      # for x in range(1, n+1):
+      #   spiral[-x].insert(0, ta[x-1])
       c += n + 1
       # bottom-left-> bottom-right
       spiral.append(list(range(c, c + n + 1 + 1)))
       c_diags.extend([spiral[-1][0], spiral[-1][-1]])
-      n_diag_primes += sum([d in primes for d in c_diags])
-      if n_diag_primes/(len(spiral[0])*2 - 1) < 0.10:
+      n_diag_primes += sum([sympy.isprime(d) for d in c_diags])
+      if (perc := n_diag_primes/(len(spiral[0])*2 - 1)) < 0.10:
         print(len(spiral[0]))
         break
+      print(perc)
+      n += 2
     except IndexError:
       pa(spiral)
       raise IndexError
-    n += 2
-  print(layer[-1])
 
 pa = lambda A: print('\n'.join([''.join(['{:4}'.format(item) for item in row])\
                   for row in A]))
