e7bbe18c8a1f3d1f4d424b2d17bdbd5b665175e3
Asim <asimalilatif@gmail.com>
2022-01-09 15:30:30 +0500

    one layer

--- 51-100/58.py
+++ 51-100/58.py
@@ -7,13 +7,30 @@ from primes import *
 def spiral_gen():
   # primes = psieve_list(10**4)
   spiral = list()
+  spiral.append([1])
   n = 1
-  while n <= 10:
-    m = n + len(spiral)
-    spiral.append([i for i in range(n)])
-    spiral.insert(spiral[n-1][0], [n+1])
+  while n <= 3:
+    print(n)
+    outpoint = n**2
+    next_outpoint = (n+2)**2
+    layer = list(range(outpoint, next_outpoint))
+    try:
+      c = outpoint
+      for x in range(1, n+1):
+        spiral[-x].append(outpoint+x)
+      c = c + 1
+      spiral.insert(0, list(range(c+3, c, -1)))
+      c = c + outpoint + 2
+      for x in range(1, n+1):
+        spiral[-x].insert(0, c+x)
+      c = c + outpoint + 1
+      spiral.append(list(range(c, c+3)))
+      print(f'spiral {spiral}')
+    except IndexError:
+      print(f'spiral {spiral}')
+      raise IndexError
+    print(f'layer {layer}')
     n += 2
-
   print(spiral)
 
 if __name__ == '__main__':
