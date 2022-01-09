06b6f051f6e9e3ce2f246a00d623ea0f6808daa5
Asim <asimalilatif@gmail.com>
2022-01-09 16:06:43 +0500

    spiraling spirals

--- 51-100/58.py
+++ 51-100/58.py
@@ -9,30 +9,36 @@ def spiral_gen():
   spiral = list()
   spiral.append([1])
   n = 1
-  while n <= 3:
+  while n < 10:
     print(n)
     outpoint = n**2
     next_outpoint = (n+2)**2
     layer = list(range(outpoint, next_outpoint))
     try:
       c = outpoint
+      # right-bottom -> right-up
       for x in range(1, n+1):
         spiral[-x].append(outpoint+x)
-      c = c + 1
-      spiral.insert(0, list(range(c+3, c, -1)))
-      c = c + outpoint + 2
+      c += n
+      # right-up -> left-up
+      spiral.insert(0, list(range(c + n + 1 + 1, c, -1)))
+      c += n + 1 + 1
+      # left-up -> left-bottom
+      ta = [c+x for x in range(1, n+1)][::-1]
       for x in range(1, n+1):
-        spiral[-x].insert(0, c+x)
-      c = c + outpoint + 1
-      spiral.append(list(range(c, c+3)))
-      print(f'spiral {spiral}')
+        spiral[-x].insert(0, ta[x-1])
+      c += n + 1
+      # left-bottom -> right-bottom
+      spiral.append(list(range(c, c + n + 1 + 1)))
+      pa(spiral)
     except IndexError:
-      print(f'spiral {spiral}')
+      pa(spiral)
       raise IndexError
-    print(f'layer {layer}')
+    print(f'\nlayer {layer}')
     n += 2
-  print(spiral)
 
+pa = lambda A: print('\n'.join([''.join(['{:4}'.format(item) for item in row])\
+                  for row in A]))
 if __name__ == '__main__':
   spiral_gen()
 
