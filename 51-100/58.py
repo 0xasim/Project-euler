3bc8f817b8a95747731f659e0fea755b811284a1
Asim <asimalilatif@gmail.com>
2022-01-09 17:51:15 +0500

    takes 15 min to compute

--- 51-100/58.py
+++ 51-100/58.py
@@ -1,9 +1,5 @@
 import numpy as np
-import math
 import sympy
-import sys
-sys.path.append('../algorithms')
-from primes import *
 
 def spiral_primes():
   spiral = [[1]]
@@ -19,22 +15,28 @@ def spiral_primes():
       for x in range(1, n+1):
         spiral[-x].append(c + x)
       c += n
+
       # top-right -> top-left
       spiral.insert(0, list(range(c + n + 1 + 1, c, -1)))
       c += n + 1 + 1
       c_diags.extend([spiral[0][0], spiral[0][-1]])
+
       # top-left -> bottom-left
-      # ta = [c+x for x in range(1, n+1)][::-1]
-      # for x in range(1, n+1):
-      #   spiral[-x].insert(0, ta[x-1])
+      '''  Tryna save compute
+      ta = [c+x for x in range(1, n+1)][::-1]
+      for x in range(1, n+1):
+        spiral[-x].insert(0, ta[x-1])
       c += n + 1
+      '''
       # bottom-left-> bottom-right
       spiral.append(list(range(c, c + n + 1 + 1)))
+
       c_diags.extend([spiral[-1][0], spiral[-1][-1]])
       n_diag_primes += sum([sympy.isprime(d) for d in c_diags])
       if (perc := n_diag_primes/(len(spiral[0])*2 - 1)) < 0.10:
         print(len(spiral[0]))
         break
+
       print(perc)
       n += 2
     except IndexError:
@@ -44,5 +46,7 @@ def spiral_primes():
 pa = lambda A: print('\n'.join([''.join(['{:4}'.format(item) for item in row])\
                   for row in A]))
 if __name__ == '__main__':
-  spiral_primes()
+  import sys ; sys.path.append('..')
+  from utils import call
+  call(spiral_primes)
 
