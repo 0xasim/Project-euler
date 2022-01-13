498c96533599e3536202297257dc80abda1084ec
Asim <asimalilatif@gmail.com>
2022-01-13 18:14:03 +0500

    close to 59 in go: :

--- 51-100/59.go
+++ 51-100/59.go
@@ -6,6 +6,11 @@ import (
 	// "bytes"
 	// "reflect"
 )
+func Use(vals ...interface{}) {
+  for _, val := range vals {
+    _ = val
+  }
+}
 
 func readFile(fname string) (int, []byte) {
 	file, _ := os.Open(fname)
@@ -14,7 +19,6 @@ func readFile(fname string) (int, []byte) {
 	count, _ := file.Read(data)
 	return count, data
 }
-
 func splitStr(data []byte, delimiter byte) []int {
   two_dim := make([]int, len(data)/2)
   x, start := 0, 0
@@ -28,14 +32,34 @@ func splitStr(data []byte, delimiter byte) []int {
   two_dim[x], _ = strconv.Atoi(string(data[start:start+2]))
   return two_dim[:x+1]
 }
-
-func xor(data []int, key string){
+func stringToBin(s string) string {
+  var binString string
+  for _, c := range s {
+    binString = fmt.Sprintf("%s%b", binString, c)
+  }
+  return binString
+}
+func xor(b1 byte, b2 byte) byte {
+  if b1 != b2 {
+    return 1
+  } 
+  return 0
+}
+func decrypt(data []int, key string){
+  bin_key := stringToBin(key)
+  var l int = 0
   for o := range data {
-    t := strconv.FormatInt(int64(data[o]), 2)
-    fmt.Printf("%s,", t)
+    bin_d1 := strconv.FormatInt(int64(data[o]), 2)
+    for p := range bin_d1 {
+      bin_d2 := bin_d1[p]
+      result := xor(bin_d2, bin_key[l])
+      fmt.Printf("%b,%b: %b\t", bin_d2, bin_key[l], result)
+      l++
+      if l > len(bin_key)-1 { l = 0 }
+    }
+    println()
   }
 }
-
 func main() {
 	_ , data := readFile("/Users/dude/fun/project_euler/data/p059_cipher.txt")
   splitted := splitStr(data, []byte(",")[0])
@@ -44,10 +68,13 @@ func main() {
   for u := range alph {
     for v := range alph {
       for w := range alph {
-        println(u+v+w)
+        // t := alph[u]+alph[v]+alph[w]
+        Use(v,w)
+        fmt.Printf("%x ", alph[u])
+        //decrypt(splitted, t)
       }
     }
   }
-  xor(splitted, "asd")
+  Use(splitted)
 }
 
